f6b0b08035addbff30fad2afe0121dae24072803
MariaSchueler <122924056+MariaSchueler@users.noreply.github.com>
2023-04-03 11:07:28 +0200

    Initial commit

---
+++ com/akamea/generator/Main.java
@@ -0,0 +1,7 @@
+package com.akamea.generator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\tBitte geben Sie den Namen der Mutter an: ");
+    }
+}
